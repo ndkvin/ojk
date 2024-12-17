688fed02e933de09ff3d5743b72d0e427bfaff0f
ndkvin <andika.kavin@gmail.com>
2024-12-17 10:13:35 +0700

    add dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Analisis;
+use App\Models\Bidang;
+use App\Models\Fungsi;
+use App\Models\IPA;
+use App\Models\Kano;
+use App\Models\Satker;
+use App\Models\SSI;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -21,8 +29,67 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('pages.home');
+
+        $function_id =  $request->get('function_id');
+        $type_id = $request->get('type_id');
+        $satker_id = $request->get('satker_id');
+        $bidang_id = $request->get('bidang_id');
+
+        $ssi = null;
+        $kano = null;
+        $ipa = null;
+        $analisis = null;
+
+        if($function_id && $type_id && $satker_id && $bidang_id) {
+            $ssi = SSI::where('function_id', $function_id)
+            ->where('type_id', $type_id)
+            ->where('satker_id', $satker_id)
+            ->where('bidang_id', $bidang_id)
+            ->first();
+
+            $kano = Kano::where('function_id', $function_id)
+            ->where('bidang_id', $bidang_id)
+            ->where('satker_id', $satker_id)
+            ->where('type_id', $type_id)
+            ->get()
+            ->map(function ($item) {
+                return [
+                    'x' => $item->puas,  // Misalnya kolom puas sebagai X-axis
+                    'y' => $item->penting, // Misalnya kolom penting sebagai Y-axis
+                    'label' => $item->attribute, // Label untuk setiap titik
+                ];
+            });
+
+            $ipa = IPA::where('function_id', $function_id)
+                ->where('bidang_id', $bidang_id)
+                ->where('satker_id', $satker_id)
+                ->where('type_id', $type_id)
+                ->get();
+
+            $analisis = Analisis::where('function_id', $function_id)
+                ->where('bidang_id', $bidang_id)
+                ->where('satker_id', $satker_id)
+                ->where('type_id', $type_id)
+                ->first();
+        }
+    
+
+        $functions = Fungsi::all();
+        $types = Type::all();
+        $satkers = Satker::all();
+        $bidangs = Bidang::all();
+
+        return view('pages.home', [
+            'functions' => $functions,
+            'types' => $types,
+            'satkers' => $satkers,
+            'bidangs' => $bidangs,
+            'ssi' => $ssi,
+            'kano' => $kano,
+            'ipa' => $ipa,
+            'analisis' => $analisis,
+        ]);
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -18,19 +18,328 @@
                 </div>
             </div>
         </div>
-
-        <section class="section">
-            <div class="card">
-                <div class="card-header">
-                    @auth
-                        @if (auth()->user()->role == 'user')
+        @auth
+            @if (auth()->user()->role == 'user')
+                <section class="section">
+                    <div class="card">
+                        <div class="card-header">
                             <h5 class="card-title">
                                 Tunggu Admin Verifikasi Data Anda
                             </h5>
-                        @endif
-                    @endauth
+                        </div>
+                    </div>
+                </section>
+            @endif
+        @endauth
+
+        <section id="multiple-column-form">
+            <div class="row match-height">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-content">
+                            <div class="card-body">
+                                <form class="form" method="GET">
+                                    <div class="row" id="form-rows">
+                                        <div class="col-md-3 col-12">
+                                            <div class="form-group">
+                                                <label for="function_id" class="form-label">Fungsi</label>
+                                                <div class="dropdown-container">
+                                                    <div class="dropdown">
+                                                        <button type="button" class="dropdown-btn"
+                                                            onclick="toggleDropdown('functionDropdown')">
+                                                            Pilih Fungsi
+                                                        </button>
+                                                        <div class="dropdown-content" id="functionDropdown">
+                                                            <input type="text" class="dropdown-search"
+                                                                placeholder="Search..." oninput="filterDropdown(this)">
+                                                            <ul class="dropdown-list">
+                                                                @foreach ($functions as $function)
+                                                                    <li data-value="{{ $function->id }}"
+                                                                        onclick="selectDropdownItem(this, 'function_id')">
+                                                                        {{ $function->function }}
+                                                                    </li>
+                                                                @endforeach
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                    <input type="hidden" id="function_id" name="function_id" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-3 col-12">
+                                            <div class="form-group">
+                                                <label for="type_id" class="form-label">Tipe</label>
+                                                <div class="dropdown-container">
+                                                    <div class="dropdown">
+                                                        <button type="button" class="dropdown-btn"
+                                                            onclick="toggleDropdown('typeDropdown')">
+                                                            Pilih Tipe
+                                                        </button>
+                                                        <div class="dropdown-content" id="typeDropdown">
+                                                            <input type="text" class="dropdown-search"
+                                                                placeholder="Search..." oninput="filterDropdown(this)">
+                                                            <ul class="dropdown-list">
+                                                                @foreach ($types as $type)
+                                                                    <li data-value="{{ $type->id }}"
+                                                                        onclick="selectDropdownItem(this, 'type_id')">
+                                                                        {{ $type->type }}
+                                                                    </li>
+                                                                @endforeach
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                    <input type="hidden" id="type_id" name="type_id" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-3 col-12">
+                                            <div class="form-group">
+                                                <label for="satker_id" class="form-label">Satuan Kerja</label>
+                                                <div class="dropdown-container">
+                                                    <div class="dropdown">
+                                                        <button type="button" class="dropdown-btn"
+                                                            onclick="toggleDropdown('satkerDropdown')">
+                                                            Pilih Satuan Kerja
+                                                        </button>
+                                                        <div class="dropdown-content" id="satkerDropdown">
+                                                            <input type="text" class="dropdown-search"
+                                                                placeholder="Search..." oninput="filterDropdown(this)">
+                                                            <ul class="dropdown-list">
+                                                                @foreach ($satkers as $satker)
+                                                                    <li data-value="{{ $satker->id }}"
+                                                                        onclick="selectDropdownItem(this, 'satker_id')">
+                                                                        {{ $satker->satker }}
+                                                                    </li>
+                                                                @endforeach
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                    <input type="hidden" id="satker_id" name="satker_id" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-3 col-12">
+                                            <div class="form-group">
+                                                <label for="bidang_id" class="form-label">Bidang</label>
+                                                <div class="dropdown-container">
+                                                    <div class="dropdown">
+                                                        <button type="button" class="dropdown-btn"
+                                                            onclick="toggleDropdown('bidangDropdown')">
+                                                            Pilih Bidang
+                                                        </button>
+                                                        <div class="dropdown-content" id="bidangDropdown">
+                                                            <input type="text" class="dropdown-search"
+                                                                placeholder="Search..." oninput="filterDropdown(this)">
+                                                            <ul class="dropdown-list">
+                                                                @foreach ($bidangs as $bidang)
+                                                                    <li data-value="{{ $bidang->id }}"
+                                                                        onclick="selectDropdownItem(this, 'bidang_id')">
+                                                                        {{ $bidang->bidang }}
+                                                                    </li>
+                                                                @endforeach
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                    <input type="hidden" id="bidang_id" name="bidang_id" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <button type="submit" class="d-none" id="submit"></button>
+                                    </div>
+                                </form>
+                                <div class="col-12 d-flex justify-content-end">
+                                    <button type="submit" onclick="submit()"
+                                        class="btn btn-primary me-1 mb-1">Filter</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
-    </div>
-@endsection
+        <h2>
+            <center>SSI</center>
+        </h2>
+        <div class="row">
+            <div class="col-md-6 col-12">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-body">
+                            <center>
+                                <h4 class="card-title">Direct</h4>
+                            </center>
+                            <center>
+                                <h5 class="card-text">
+                                    {{ $ssi == null ? 'Tidak Ada Data' : $ssi->rp * 0.3 + $ssi->pd * 0.3 + $ssi->os * 0.3 + $ssi->af * 0.1 }}
+                                </h5>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-12">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-body">
+                            <center>
+                                <h4 class="card-title">Indirect</h4>
+                            </center>
+                            <center>
+                                <h5 class="card-text">
+                                    {{ $ssi == null ? 'Tidak Ada Data' : $ssi->indirect_os * 0.9 + $ssi->indirect_af * 0.1 }}
+                                </h5>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="mx-auto col-md-8 col-12">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-body">
+                            <center>
+                                <h4 class="card-title">Total</h4>
+                            </center>
+                            <center>
+                                <h5 class="card-text">
+                                    {{ $ssi == null ? 'Tidak Ada Data' : ($ssi->rp * 0.3 + $ssi->pd * 0.3 + $ssi->os * 0.3 + $ssi->af * 0.1) * 0.6 + ($ssi->indirect_os * 0.9 + $ssi->indirect_af * 0.1) * 0.4 }}
+                                </h5>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <section id="multiple-column-form">
+            <h3>
+                <center>Kano</center>
+            </h3>
+            <div class="row match-height">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-content">
+                            <div class="card-body">
+                                <center>
+                                    <h5 class="card-text">
+                                        {{ $kano == null ? 'Tidak Ada Data' : ($ssi->rp * 0.3 + $ssi->pd * 0.3 + $ssi->os * 0.3 + $ssi->af * 0.1) * 0.6 + ($ssi->indirect_os * 0.9 + $ssi->indirect_af * 0.1) * 0.4 }}
+                                    </h5>
+                                </center>
+                                @if (session('success'))
+                                    <div>{{ session('success') }}</div>
+                                @endif
+                                <div>
+                                    <canvas id="quadrantChart"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    @endsection
+
+    @section('scripts')
+        <script>
+            function submit() {
+                const button = document.getElementById('submit');
+
+                button.click();
+            }
+        </script>
+        <script>
+            // Data dari Controller
+            const chartData = @json($kano);
+
+            function getRandomColor() {
+                const r = Math.floor(Math.random() * 255);
+                const g = Math.floor(Math.random() * 255);
+                const b = Math.floor(Math.random() * 255);
+                return `rgba(${r}, ${g}, ${b}, 0.8)`; // Transparansi 0.8
+            }
+
+            // Tambahkan warna random ke setiap titik dalam data
+            const dataWithColors = chartData.map(point => ({
+                ...point,
+                backgroundColor: getRandomColor()
+            }));
+
+            const ctx = document.getElementById('quadrantChart').getContext('2d');
+            new Chart(ctx, {
+                type: 'scatter',
+                data: {
+                    datasets: [{
+                        label: '',
+                        data: dataWithColors,
+                        pointBackgroundColor: dataWithColors.map(point => point.backgroundColor),
+                        radius: 10,
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    scales: {
+                        x: {
+                            min: -6,
+                            max: 6,
+                            title: {
+                                display: true,
+                                text: 'Puas (X-Axis)'
+                            },
+                            grid: {
+                                drawBorder: true,
+                                color: function(context) {
+                                    return context.tick.value === 0 ? '#000' : '#ddd';
+                                }
+                            }
+                        },
+                        y: {
+                            min: -6,
+                            max: 6,
+                            title: {
+                                display: true,
+                                text: 'Penting (Y-Axis)'
+                            },
+                            grid: {
+                                drawBorder: true,
+                                color: function(context) {
+                                    return context.tick.value === 0 ? '#000' : '#ddd';
+                                }
+                            }
+                        }
+                    },
+                    plugins: {
+                        tooltip: {
+                            backgroundColor: 'rgba(0, 0, 0, 0.8)', // Warna background tooltip
+                            titleColor: '#ffffff', // Warna judul
+                            bodyColor: '#ffffff', // Warna isi teks
+                            titleFont: {
+                                size: 16,
+                                weight: 'bold'
+                            }, // Styling font title
+                            bodyFont: {
+                                size: 14
+                            }, // Styling font body
+                            padding: 12, // Padding dalam tooltip
+                            cornerRadius: 8, // Sudut melengkung tooltip
+                            boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.3)', // Bayangan (custom CSS)
+                            callbacks: {
+                                label: function(context) {
+                                    const dataPoint = context.raw;
+                                    return [
+                                        `Attribute: ${dataPoint.label}`,
+                                        `Penting: ${dataPoint.y}`,
+                                        `Puas: ${dataPoint.x}`
+                                    ];
+                                }
+                            }
+                        }
+                    },
+                    hover: {
+                        mode: 'nearest',
+                        intersect: true
+                    }
+                }
+            });
+        </script>
+    @endsection
